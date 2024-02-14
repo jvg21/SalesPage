984d92e79623241e205020caf1d0de907af915b3
João Gregorini <75985123+jvg21@users.noreply.github.com>
2024-02-14 15:44:23 -0300

    Dropdown1

--- src/components/SalesCard.tsx
+++ src/components/SalesCard.tsx
@@ -1,23 +1,39 @@
+import { useWindowSize } from "@uidotdev/usehooks";
+import { useEffect, useState } from "react";
 import styled from "styled-components"
 
-const SaleCard = styled.div`
+const Div2 = styled.div<{display:boolean}>`
+    width: 100%;
+    display: ${(props)=> props.display?'flex':'none'};
+    justify-content: space-evenly;
+    flex-direction: column;
+    align-items: center;
+`;
 
-    background-color: #efefef;
-    height: 400px;
+
+const SaleCard = styled.div<{dropdown:boolean}>`
+
+    background-color: white;
+    height: ${(props)=> props.dropdown?'180px':'450px'};
     width: 300px;
     display: flex;
     justify-content: space-between;
     align-items: center;
     flex-direction: column;
+    padding-bottom: 5px;
+    transition: all 1s ease;
+    border:#9B1620 ;
 
     @media screen and (max-width: 450px) {
         margin: 0 3vw;
         width:90vw
     }
+
 `;
 
 const SalesTittle = styled.div`
-    background-color: red;
+    background-color: #9B1620;
+    overflow: hidden;
     color: white;
     font-weight: bold;
     width: 100%;
@@ -25,46 +41,92 @@ const SalesTittle = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    padding: 0px 10px;
 `;
+
 
 const SalesButton = styled.button`
     width: 80%;
     height: 40px;
     font-size: 1.2em;
-    background-color: green;
+    background-color: #003F1D;
     color: white;
     border: solid 2px;
     border-color: black;
-    margin: 15px 0;
-    transition: all 1s ;
+    border-radius: 7px;
+
     &:hover{
-        background-color: greenyellow;
+        background-color: #015528;
     }
 
 `;
 
-const SalesText = styled.p`
-
+const SalesButtonInfo = styled(SalesButton)`
+    background-color: #dbdb03;
+    color: black;
+`;
+const SalesTittleText = styled.p`
+    display: flex;
+    text-align: center;
 
 `;
 
-export default function SalesCard(props: { nome: string, preco: string }) {
+const SalesText = styled.p``;
+const SalesPriceText = styled.p`
+
+`;
+
+const ButtonDiv = styled.div`
+    /* background-color: blue; */
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 5px 0;
+
+`;
+
+
+export default function SalesCard(props: { titulo1:string ,titulo2: string, preco: string}) {
     const preco = `R$ ${props.preco}`
 
+    const [dropdown,setDropdown] = useState(false);
+    const size = useWindowSize();
+
+    
+    useEffect(()=>{
+        if(size.height && size.height<=280) 
+    {setDropdown(true)}
+    else{
+        setDropdown(false)
+    }
+    },[size])
+    
     return (
         <>
-        <SaleCard>
+        <SaleCard dropdown={dropdown}>
             <SalesTittle>
-                <h2>{props.nome}</h2>
+                <SalesTittleText>{props.titulo2}</SalesTittleText>
             </SalesTittle>
             <h1>{preco}</h1>
-            <SalesTittle>
-                <h2>{props.nome}</h2>
-            </SalesTittle>
-            <h1>{preco}</h1>
-            <SalesText>"2º LOTE ATÉ 20/3/2024<br/>*Enquanto houver vagas<br/> 3º LOTE: R$350,00 / R$450,00"</SalesText>
+
+
+            <Div2 display={!dropdown}>
+                <SalesTittle>
+                    <SalesTittleText>{props.titulo1}</SalesTittleText>
+                </SalesTittle>
+                <div style={{height:'20px'}}></div>
+                <h1>{preco}</h1>
+                <div style={{height:'20px'}}></div>
+                <SalesText>2º LOTE ATÉ 20/3/2024<br/>*Enquanto houver vagas<br/> 3º LOTE: R$350,00 / R$450,00</SalesText>
+            </Div2>
             
-            <SalesButton>Comprar</SalesButton>
+            <ButtonDiv>
+                <SalesButton>Comprar</SalesButton>
+                <SalesButtonInfo>Info</SalesButtonInfo>
+            </ButtonDiv>
+            
         </SaleCard>
 
         </>
--- src/components/SalesPage.tsx
+++ src/components/SalesPage.tsx
@@ -1,16 +1,17 @@
 import styled from "styled-components"
 import SalesCard from "./SalesCard";
+import { useRef } from "react";
 
 const SalesDiv = styled.div`
 
-    background-color: red;
+    background-color: #9B1620;
     width: 100%;
     height: 75vh;
 
 `;
 
 const SalesCardView = styled.div`
-    background-color: darkblue;
+    background-color: #000000;
     padding: 5px 10px;
     display: flex;
     justify-content: center;
@@ -18,15 +19,28 @@ const SalesCardView = styled.div`
     flex-wrap: wrap;
     gap: 4vh 10vw;
 `;
-export default function SalesPage(){
+export default function SalesPage() {
 
-    return(
+    const ref = useRef<HTMLDivElement | null>(null);
+
+    const handleClick = () => {
+        ref.current?.scrollIntoView({ behavior: 'smooth' });
+    };
+
+
+    return (
         <>
-        {/* <SalesDiv></SalesDiv> */}
-        <SalesCardView>
-            <SalesCard nome={"ACADEMICO"} preco={'250,00'}></SalesCard>        
-            <SalesCard nome={"PROFISSIONAL"} preco={'350,00'}></SalesCard>        
-        </SalesCardView>
+            <div style={{ width: '100%', height: '80px', background: 'black' }}>
+                <button onClick={handleClick}>sadads</button>
+
+            </div>
+            <SalesDiv> </SalesDiv>
+            <SalesCardView ref={ref}>
+                <SalesCard titulo1={"ACADÊMICOS DE GRADUAÇÃO CONVENIADOS"} titulo2={"ACADÊMICOS DE GRADUAÇÃO NÃO CONVENIADOS"} preco={'250,00'}></SalesCard>
+                <SalesCard titulo1={"PROFISSIONAIS CONVENIADOS"} titulo2={"PROFISSIONAIS NÃO CONVENIADOS"} preco={'350,00'}></SalesCard>
+            </SalesCardView>
+            <SalesDiv> </SalesDiv>
+
         </>
     )
 }
